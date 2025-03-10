d597d371dad79c861c5649df00f653fb86376aa3
sanjivthapa <thapasvt12@gmail.com>
2025-03-10 17:05:41 +0545

    updated code for get, post, put, delete methods

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -1,33 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import UUID, uuid4
+from typing import Optional
+
 app = FastAPI()
 
 class Tasks(BaseModel):
-    id: UUID | None = None
+    id: Optional[UUID] = None
     title: str
     description: str | None = None
     completed: bool = False
     
 tasks = []
 
-@app.post("/tasks/", response_model=list[Tasks])
+@app.post("/tasks/", response_model=Tasks)
 def create_tasks(task: Tasks):
     new_task = task.model_copy(update={"id": uuid4()})
     tasks.append(new_task)
-    return new_task
+    return new_task.model_dump()
 
 @app.get("/tasks/", response_model = list[Tasks])
 def read_tasks():
     return tasks
 
+@app.get("/tasks/{task_id}", response_model=Tasks)
+def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
 
-#you can send curl request to test api
-# curl -X POST http://127.0.0.1:8000/tasks/ \         
-#      -H "Content-Type: application/json" \
-#      -d '{"title": "OK it is working now yayayayayyayay", "description": "its workign bang bang bang", "completed": true}'
+@app.put("/tasks/{task_id}", response_model=Tasks)
+def update_task(task_id: UUID, task_update: Tasks):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.model_dump(exclude_unset=True))
+            tasks[index] = updated_task
+            return updated_task
+    raise HTTPException(status_code=404, detail="Item not found")
 
-
-#can also do get reqest
-
-# curl -X GET http://127.0.0.1:8000/tasks/
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted successfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
