33be212d12fe9b8fcb42db89eaca2b2d37808fe0
sanjivthapa <thapasvt12@gmail.com>
2025-03-11 15:53:17 +0545

    added database sqlite and everything is working now bang

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -1,48 +1,77 @@
-from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import UUID, uuid4
-from typing import Optional
+from typing import Optional, Annotated, Union
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends, FastAPI, HTTPException, Query
+
+class Tasks(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    description: str = Field()
+    completed: bool = Field(default=False)
+    
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 app = FastAPI()
 
-class Tasks(BaseModel):
-    id: Optional[UUID] = None
-    title: str
-    description: str | None = None
-    completed: bool = False
-    
-tasks = []
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-@app.post("/tasks/", response_model=Tasks)
-def create_tasks(task: Tasks):
-    new_task = task.model_copy(update={"id": uuid4()})
-    tasks.append(new_task)
-    return new_task.model_dump()
+@app.post("/tasks/")
+def create_tasks(task: Tasks, session: SessionDep) -> Tasks:
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
 
-@app.get("/tasks/", response_model = list[Tasks])
-def read_tasks():
+@app.get("/tasks/")
+def read_tasks(session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)] = 100) -> list[Tasks]:
+    tasks = session.exec(select(Tasks).offset(offset).limit(limit)).all()
     return tasks
 
-@app.get("/tasks/{task_id}", response_model=Tasks)
-def read_task(task_id: UUID):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+@app.get("/tasks/{task_id}")
+def read_task(task_id: int, session: SessionDep) -> Tasks:
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
 
-@app.put("/tasks/{task_id}", response_model=Tasks)
-def update_task(task_id: UUID, task_update: Tasks):
-    for index, task in enumerate(tasks):
-        if task.id == task_id:
-            updated_task = task.copy(update=task_update.model_dump(exclude_unset=True))
-            tasks[index] = updated_task
-            return updated_task
-    raise HTTPException(status_code=404, detail="Item not found")
+    return task
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: Tasks, session: SessionDep) -> Tasks:
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Item not found")
+    task.title = updated_task.title
+    task.description = updated_task.description
+    task.completed = updated_task.completed
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: UUID):
-    for task in tasks:
-        if task.id == task_id:
-            tasks.remove(task)
-            return {"message": "Task deleted successfully"}
-    raise HTTPException(status_code=404, detail="Item not found")
+def delete_task(task_id: int, session: SessionDep) -> Tasks:
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    session.delete(task)
+    session.commit()
+    return {"ok": True}
