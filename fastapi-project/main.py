4df21128fb4cb25b0508e1a8529e70ee3631e714
sanjivthapa <thapasvt12@gmail.com>
2025-03-11 18:57:43 +0545

    updated code for get, post, patch, delete methods

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -4,12 +4,24 @@ from typing import Optional, Annotated, Union
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi import Depends, FastAPI, HTTPException, Query
 
-class Tasks(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class TasksBase(SQLModel):
     title: str = Field(index=True)
-    description: str = Field()
+    description: str = Field
     completed: bool = Field(default=False)
+
+class Tasks(TasksBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+class TasksPublic(TasksBase):
+    id: int
     
+class TasksCreate(TasksBase):
+    pass
+
+class TasksUpdate(TasksBase):
+    title: str | None
+    description: str | None
+    completed: bool | None
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -35,40 +47,41 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-@app.post("/tasks/")
-def create_tasks(task: Tasks, session: SessionDep) -> Tasks:
-    session.add(task)
+@app.post("/tasks/", response_model=TasksPublic)
+def create_tasks(task: TasksCreate, session: SessionDep) -> Tasks:
+    db_task = Tasks.model_validate(task)
+    session.add(db_task)
     session.commit()
-    session.refresh(task)
-    return task
+    session.refresh(db_task)
+    return db_task
 
-@app.get("/tasks/")
+@app.get("/tasks/", response_model=list[TasksPublic])
 def read_tasks(session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)] = 100) -> list[Tasks]:
     tasks = session.exec(select(Tasks).offset(offset).limit(limit)).all()
     return tasks
 
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}", response_model=TasksPublic)
 def read_task(task_id: int, session: SessionDep) -> Tasks:
     task = session.get(Tasks, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
-    return task
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int, updated_task: Tasks, session: SessionDep) -> Tasks:
-    task = session.get(Tasks, task_id)
-    if not task:
+@app.patch("/tasks/{task_id}", response_model=TasksPublic)
+def update_task(task_id: int, task: TasksUpdate, session: SessionDep) -> Tasks:
+    task_db = session.get(Tasks, task_id)
+    if not task_db:
         raise HTTPException(status_code=404, detail="Item not found")
-    task.title = updated_task.title
-    task.description = updated_task.description
-    task.completed = updated_task.completed
-    session.add(task)
+   
+    task_data = task.model_dump(exclude_unset=True)
+    task_db.sqlmodel_update(task_data)
+    session.add(task_db)
     session.commit()
-    session.refresh(task)
-    return task
+    session.refresh(task_db)
+    return task_db
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int, session: SessionDep) -> Tasks:
+def delete_task(task_id: int, session: SessionDep):
     task = session.get(Tasks, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
